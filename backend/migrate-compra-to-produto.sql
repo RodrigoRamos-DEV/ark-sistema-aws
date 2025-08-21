dc28b75e938d77633aebc795c187c37ebc2d5cee
RodrigoRamos-DEV <rodrigomramos18@gmail.com>
2025-08-21 17:02:02 -0300

    delete

--- backend/migrate-compra-to-produto.sql
+++
@@ -1,6 +0,0 @@
--- Migração: alterar todos os itens do tipo 'compra' para 'produto'
-UPDATE items SET type = 'produto' WHERE type = 'compra';
-
--- Verificar resultado
-SELECT COUNT(*) as total_produtos FROM items WHERE type = 'produto';
-SELECT COUNT(*) as total_compras FROM items WHERE type = 'compra';
